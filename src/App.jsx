244b127d6c2e3cbf40668293c6208374b05830ed
NhatHONGUYEN <hnnhat67@gmail.com>
2024-07-31 10:40:19 +0200

    ✨ F01 : Create our components OFFER , GALLERY , INFORMATIONS , PROGRAMSESSION and adding the route for them

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Offer from "./components/Offer";
+import Gallery from "./components/Gallery";
+import Informations from "./components/Informations";
+import ProgramSession from "./components/ProgramSession";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/offer" element={<Offer />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/informations" element={<Informations />} />
+        <Route path="/programSession" element={<ProgramSession />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/ProgramSession.jsx
@@ -0,0 +1,3 @@
+export default function ProgramSession() {
+  return <div>ProgramSession</div>;
+}
